3759863e8f6c14136b77ca6b30f3c8a3d8b6cfe8
Omri Levy <omri2861@gmail.com>
2020-11-03 00:18:43 +0200

    Moved the play sound to a function and added support for keydowns

--- DrumKit/index.js
+++ DrumKit/index.js
@@ -10,13 +10,32 @@ soundMap.set("l", "sounds/crash.mp3");
 
 
 function handleClick() {
-    let soundUrl = soundMap.get(this.innerHTML);
+    playSound(this.innerHTML);
+}
+
+function handleKeyDown(event) {
+    // Note that we have to accept the event as a parameter, since 'this' is now
+    // set to the global 'document' object
     
-    if (soundUrl === undefined) {
-        console.warn("Unexpected key press: " + this.innerHTML);
+    if (!soundMap.has(event.key)) {
+        // We don't have a drum assigned for this key, so just skip it
         return;
     }
 
+    playSound(event.key);
+}
+
+/*
+ * Plays a drum sound for a single character (either key pressed or clicked).
+ */
+function playSound(character) {
+    if (!soundMap.has(character)) {
+        console.error("Error: playSound called with non-mapped character: " + character);
+        return;
+    }
+
+    let soundUrl = soundMap.get(character);
+    
     let audio = new Audio(soundUrl);
     audio.play();
 }
@@ -25,3 +44,4 @@ for (button of document.querySelectorAll(".drum")) {
     button.addEventListener("click", handleClick);
 }
 
+document.addEventListener("keydown", handleKeyDown);
