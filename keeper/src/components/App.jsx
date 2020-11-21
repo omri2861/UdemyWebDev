743ceeece3a4bcd78dba3c2ff34d15b54175850c
Omri Levy <omri2861@gmail.com>
2020-11-22 01:06:03 +0200

    Implemented the add note option and fixed CreateArea indentation

--- keeper/src/components/App.jsx
+++ keeper/src/components/App.jsx
@@ -4,15 +4,48 @@ import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
+// This key will represent each note. It's better than an index, since if an item gets deleted,
+// the next created item will not receive it's ID, ensuring each one is unique.
+// This is why it also must be a global in the scope of the application.
+var notesKey = 1;
+
 function App() {
-  return (
-    <div>
-      <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
-      <Footer />
-    </div>
-  );
+    const [notes, setNotes] = React.useState([
+        {
+            key: 0,
+            title: "Note Title",
+            content: "Note content",
+        },
+    ]);
+
+    function addNote(title, content) {
+        setNotes((prevNotes) => {
+            return [
+                ...prevNotes,
+                {
+                    key: notesKey,
+                    title: title,
+                    content: content,
+                },
+            ];
+        });
+        notesKey++;
+    }
+
+    return (
+        <div>
+            <Header />
+            <CreateArea onSubmission={addNote} />
+            {notes.map((note) => (
+                <Note
+                    key={note.key}
+                    title={note.title}
+                    content={note.content}
+                />
+            ))}
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- keeper/src/components/CreateArea.jsx
+++ keeper/src/components/CreateArea.jsx
@@ -1,15 +1,46 @@
 import React from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  const [title, setTitle] = React.useState("");
+  const [content, setContent] = React.useState("");
+
+  function updateForm(event) {
+    const { name, value } = event.target;
+
+    if (name === "title") {
+      setTitle(value);
+    } else if (name === "content") {
+      setContent(value);
+    }
+  }
+
+  function submit(event) {
+    props.onSubmission(title, content);
+    setTitle("");
+    setContent("");
+    event.preventDefault();
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input
+          name="title"
+          placeholder="Title"
+          value={title}
+          onChange={updateForm}
+        />
+        <textarea
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          value={content}
+          onChange={updateForm}
+        />
+        <button onClick={submit}>Add</button>
       </form>
     </div>
-  );
+    );
 }
 
 export default CreateArea;
