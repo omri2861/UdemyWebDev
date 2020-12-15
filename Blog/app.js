f74055be17071b2ef41be9f9475c38e2513ace5c
Omri Levy <omri2861@gmail.com>
2020-12-15 17:13:36 +0200

    Added challange 9 solution

--- Blog/app.js
+++ Blog/app.js
@@ -35,6 +35,11 @@ app.get("/compose", (req, res) => {
   res.render("compose");
 });
 
+app.post("/compose", (req, res) => {
+  console.log("# " + req.body.postTitle);
+  console.log(req.body.postBody);
+});
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
